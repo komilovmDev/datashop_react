2cd12c89435bf3a3058de7a97f7048b9d730ba59
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-06-17 02:08:30 +0500

    react routes add

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Router.jsx'
+import './App.css'
+import Navbar from './components/navbar/Navbar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+  <>
+    <Navbar />
+    <RouterProvider router={router} />
+  </>
 )
