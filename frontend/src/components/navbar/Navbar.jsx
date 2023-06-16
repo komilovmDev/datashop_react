e4200e8bfc5682369958bc05420d0c4c923c7185
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-06-17 02:21:17 +0500

    nav st

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,7 +3,18 @@
 export default function Navbar() {
     return (
         <nav>
-            hello navbar
+            <div className="navTop">
+                <div className="navTopLeft">
+                    <span>Адрес: Urganch, Darital, 2-этаж</span>
+                </div>
+                <div className="navTopRight">
+                    <span>Телефон поддержки: (99) 900 10 10</span>
+                    <div className="language">
+                        <img src="" alt="uzbFlag" />
+                        <span>O'z</span>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
